81c039147d2f95d17aa0b623cdc57a16f8a4360b
Virgil John Lomocso <virgil@fullscale.io>
2019-03-05 13:20:05 +0800

    Initial commit

---
+++ ApplicationStartup.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms;
+
+namespace ApplicationStartup.Droid
+{
+    [Activity(Label = "ApplicationStartup", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    // ReSharper disable once UnusedMember.Global
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Forms.SetFlags("FastRenderers_Experimental");
+            Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
